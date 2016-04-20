aec49c5fd0541a30f5c01ea6cf774527b4a06443
Laurence <laurenceweening@gmail.com>
2016-04-20 11:56:06 +0200

    Create todos'

--- js/index_jquery.js
+++ js/index_jquery.js
@@ -17,4 +17,44 @@ function toggleDone() {
   updateCounters();
 }
 
+function createTodo(title) {
+  var checkboxId = "todo-" + nextTodoId();
+
+  var newTodo = $("<li></li>")
+    .addClass("todo")
+    .append($('<input>')
+      .attr('type', 'checkbox')
+      .attr('id', checkboxId)
+      .val(1)
+      .bind('change', toggleDone)
+    ).append(
+        document.createTextNode( " " )
+    ).append($('<label></label>')
+      .attr('for', checkboxId)
+      .html(title)
+    );
+
+  $("#todolist").append( newTodo );
+
+  updateCounters();
+}
+
+function nextTodoId(){
+  return $(".todo").length + 1;
+}
+
+function submitTodo(event) {
+  // stop the form from doing the default action, submitting ...
+  event. preventDefault();
+
+  var title = $("#new-todo").val()
+
+  createTodo(title);
+
+  $("#new-todo").val(null)
+  updateCounters();
+
+}
+
 $("input[type=checkbox]").bind('change', toggleDone);
+$("form").bind('submit', submitTodo);
