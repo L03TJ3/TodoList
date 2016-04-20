a1d898d4d08cd5d81a28b43a53a01892e08a89fa
Laurence <laurenceweening@gmail.com>
2016-04-20 10:38:24 +0200

    Toggle done state function

--- js/index.js
+++ js/index.js
@@ -1,17 +1,34 @@
 function updateCounters() {
   // Total number of todos
-  var todoCounter = document.getElementById('todo-count');
-  var totalTodos = document.getElementByClassName("todo").length;
-  todoCounter.innerHTML = totalTodos;
+  var totalCount = document.getElementById('total-count');
+  var totalTodos = document.getElementsByClassName("todo").length;
+  totalCount.innerHTML = totalTodos;
 
   // Total number of completed todos
   var completedCount = document.getElementById('completed-count');
-  var completedTodos = document.getElementByClassName("completed").length;
+  var completedTodos = document.getElementsByClassName("completed").length;
   completedCount.innerHTML = completedTodos;
 
   // Total number of uncompleted todos
   var todoCount = document.getElementById('todo-count');
   var uncompletedTodos = totalTodos - completedTodos;
+  todoCount.innerHTML = uncompletedTodos;
 }
 
-updateCounter();
+updateCounters();
+
+
+function toggleDone(){
+  var checkbox = this;
+
+  // check the checked status of the checkbox
+  if (checkbox.checked) {
+    // the "completed" class should be set on the parent element, the <li>
+    checkbox.parentElement.className = "todo completed";
+  }
+  else {
+    checkbox.parentElement.className = "todo";
+  }
+
+  updateCounters();
+}
